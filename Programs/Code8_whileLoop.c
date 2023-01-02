9b99e46f976555436da13b1dc365fa3d7d550fbb
DavidAlba2627 <78463209+DavidAlba2627@users.noreply.github.com>
2023-01-02 12:42:05 -0600

    Add files via upload

---
+++ Programs/Code8_whileLoop.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    float x,b,y=0;
+    printf("This program calculates the square root using babilonic algorithm\n");
+    printf("Enter the number to calculate its root\n");
+    scanf("%f", &x);
+
+    if(x>=0){
+        b = x;
+        while (b!=y)
+        {
+            y=b;
+            b=0.5*(x/b + b);
+        }
+        printf("The root is %0.4f", b);
+    }
+    if(x<0){
+        x = abs(x);
+        b = x;
+        while (b!=y)
+        {
+            y=b;
+            b=0.5*(x/b + b);
+        }
+        printf("The imaginary root is %0.4f i", b);
+    }
+
+    return 0;
+}
