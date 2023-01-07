570370da29f82bcdfa6b81e84a68e718e162028f
DavidAlba2627 <78463209+DavidAlba2627@users.noreply.github.com>
2023-01-07 12:56:55 -0600

    Update Code8_whileLoop.c

--- Programs/Code8_whileLoop.c
+++ Programs/Code8_whileLoop.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 int main()
 {
     float x,b,y=0;
@@ -17,7 +17,7 @@ int main()
         printf("The root is %0.4f", b);
     }
     if(x<0){
-        x = abs(x);
+        x = fabs(x);
         b = x;
         while (b!=y)
         {
